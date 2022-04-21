5ed3839d41b2772092403f2c4b4d3c713c1196d5
Egor Chistyakov <egchistyakov@ozon.ru>
2022-04-21 22:53:06 +0300

    [increment1] добавил роутер логгер и заготовки методов

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
+	shortener "github.com/chistopat/go-musthave-shortener/internal/app"
+	"log"
 	"net/http"
 )
 
-// HelloWorld — обработчик запроса.
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello, World</h1>"))
-}
-
 func main() {
-	// маршрутизация запросов обработчику
-	http.HandleFunc("/", HelloWorld)
-	// запуск сервера с адресом localhost, порт 8080
-	http.ListenAndServe(":8080", nil)
+	log.Printf("Server started")
+
+	router := shortener.NewRouter()
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
